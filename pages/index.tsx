3d5bfc26c8ee63d88f88cec99f055325a06d848f
Saransh27 <mehra.saransh27@gmail.com>
2023-03-30 18:18:46 +0530

    display movie list

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,10 @@ import { NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
 
 import Navbar from '../components/Navbar';
+import BillBoard from '../components/Billboard';
+import useMovies from '../hooks/useMovieList';
+import useMovieList from '../hooks/useMovieList';
+import MovieList from '../components/MovieList';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -22,9 +26,14 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 const Home = () => {
+  const { data: movies = [] } = useMovieList();
   return (
     <>
       <Navbar />
+      <BillBoard />
+      <div className='pb-40'>
+        <MovieList title='Trending Now' data={movies} />
+      </div>
     </>
   );
 };
